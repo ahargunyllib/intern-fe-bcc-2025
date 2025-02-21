a2751ebb6641aea6cc0c6ff6a6d29f022bad905f
ahargunyllib <billy.bpm03@gmail.com>
2025-02-21 23:35:46 +0700

    feat: enhance loading and error states in hero section with improved styling

--- src/features/landing/containers/hero-section-container.tsx
+++ src/features/landing/containers/hero-section-container.tsx
@@ -8,15 +8,25 @@ export default function HeroSectionContainer() {
 	const { data, error, isLoading } = useSessionQuery();
 
 	if (isLoading) {
-		return <div>Loading...</div>;
+		return (
+			<div className="mx-4 max-w-sm w-full h-fit border rounded-lg p-4 flex flex-col gap-4 shadow-md">
+				<div className="w-full h-12 bg-slate-500 rounded-md animate-pulse" />
+				<div className="w-full h-8 bg-slate-500 rounded-md animate-pulse" />
+			</div>
+		);
 	}
 
 	if (error) {
-		return <div>Error: {error.message}</div>;
+		return (
+			<div className="mx-4 max-w-sm w-full h-fit border rounded-lg p-4 flex flex-col gap-4 shadow-md">
+				<h2 className="text-lg font-semibold text-center">An error occurred</h2>
+				<p className="text-center text-red-500">{error.message}</p>
+			</div>
+		);
 	}
 
 	if (!data) {
-		return <div>Welcome, guest</div>;
+		return <NotLoggedInCard />;
 	}
 
 	const isLoggedIn = data.isLoggedIn;
