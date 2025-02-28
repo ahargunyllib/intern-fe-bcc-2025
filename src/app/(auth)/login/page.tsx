ce916f6105218187c3ffc4dc5e2a460bc53631b1
ahargunyllib <billy.bpm03@gmail.com>
2025-02-28 11:38:30 +0700

    feat: redesign login page layout and enhance login form with password visibility toggle

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,34 @@
 import LoginForm from "@/features/login/components/login-form";
+import { Blob } from "@/shared/components/icons";
+import Link from "next/link";
 
 export default function Page() {
 	return (
-		<div className="w-full h-screen flex items-center justify-center">
-			<div className="mx-4 max-w-sm w-full h-fit border rounded-lg p-4 flex flex-col gap-4 shadow-md">
-				<h1 className="text-2xl font-bold text-center">Login</h1>
-				<LoginForm />
+		<section className="h-screen w-screen grid grid-cols-2 grid-rows-1 gap-0">
+			<div className="h-screen w-screen absolute -z-0 overflow-y-hidden">
+				<Blob />
 			</div>
-		</div>
+			<div className="col-span-1 z-10 flex flex-col h-full p-16 justify-between">
+				<div className="w-2/4 aspect-square rounded-full bg-white" />
+				<div className="flex flex-col gap-4 text-white">
+					<h1 className="font-bold text-6xl">
+						Selamat datang di Locapreneur.id
+					</h1>
+					<span className="font-bold text-3xl">
+						Bersama kita, pengusaha lokal Indonesia, tumbuh dan sukses.
+					</span>
+				</div>
+			</div>
+			<div className="col-span-1 z-10 flex flex-col gap-4 h-full p-16 justify-center w-3/4">
+				<h2 className="font-bold text-7xl">Login</h2>
+				<LoginForm />
+				<span className="font-light text-xs">
+					Belum punya akun?{" "}
+					<Link href="/register" className="hover:underline font-bold text-xs">
+						Registrasi Sekarang!
+					</Link>
+				</span>
+			</div>
+		</section>
 	);
 }
--- src/features/login/components/login-form.tsx
+++ src/features/login/components/login-form.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import {
+	LucideEye,
+	LucideEyeOff,
+	LucideKeyRound,
+	LucideMail,
+} from "@/shared/components/icons";
 import Button from "@/shared/components/ui/button";
 import { FormFieldInput } from "@/shared/components/ui/form";
 import { LoginSchema } from "@/shared/repository/dto/auth";
 import { useLoginMutation } from "@/shared/repository/query/auth";
 import { Form, Formik } from "formik";
+import { useState } from "react";
 
 export default function LoginForm() {
 	const { mutate: login, isPending } = useLoginMutation();
+	const [isShowPassword, setIsShowPassword] = useState(false);
+
+	const toggleShowPassword = () => {
+		setIsShowPassword((prev) => !prev);
+	};
 
 	return (
 		<Formik
@@ -26,15 +38,30 @@ export default function LoginForm() {
 					name="email"
 					label="Email"
 					placeholder="user/admin@example.com"
+					prefixIcon={<LucideMail width={16} height={16} />}
 				/>
 				<FormFieldInput
 					name="password"
 					label="Password"
-					type="password"
+					type={isShowPassword ? "text" : "password"}
 					placeholder="password"
+					prefixIcon={<LucideKeyRound width={16} height={16} />}
+					suffixIcon={
+						<button
+							className="text-foreground/50 hover:text-foreground transition-colors duration-150 ease-in-out"
+							type="button"
+							onClick={toggleShowPassword}
+						>
+							{isShowPassword ? (
+								<LucideEyeOff width={16} height={16} />
+							) : (
+								<LucideEye width={16} height={16} />
+							)}
+						</button>
+					}
 				/>
 				<Button type="submit" disabled={isPending}>
-					Login
+					Masuk
 				</Button>
 			</Form>
 		</Formik>
