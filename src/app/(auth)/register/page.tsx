f04a0e2e24a3a433141b07736ebf62d1b740aa77
ahargunyllib <billy.bpm03@gmail.com>
2025-02-28 23:37:44 +0700

    feat: update layout of login and register pages for improved responsiveness and aesthetics

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export default function Page() {
 	return (
-		<section className="h-screen w-screen grid grid-cols-2 grid-rows-1 gap-0">
-			<div className="h-screen w-screen absolute -z-0 overflow-y-hidden">
+		<section className="h-screen w-screen grid grid-cols-1 lg:grid-cols-2 grid-rows-1 gap-0">
+			<div className="hidden lg:block h-screen w-screen absolute -z-0 overflow-y-hidden">
 				<Blob />
 			</div>
-			<div className="col-span-1 z-10 flex flex-col h-full p-16 justify-between">
+			<div className="col-span-1 z-10 hidden lg:flex flex-col h-full p-16 justify-between">
 				<div className="w-2/4 aspect-square rounded-full bg-white" />
-				<div className="flex flex-col gap-4 text-white w-3/5 text-pretty">
+				<div className="flex flex-col gap-4 text-white text-pretty">
 					<h1 className="font-bold text-6xl">
 						Selamat datang di Locapreneur.id
 					</h1>
@@ -19,10 +19,10 @@ export default function Page() {
 					</span>
 				</div>
 			</div>
-			<div className="col-span-1 z-10 flex flex-col gap-4 h-full p-16 justify-center w-3/4">
+			<div className="col-span-1 z-10 flex flex-col gap-4 h-full p-16 justify-center items-center lg:items-start lg:w-3/4">
 				<h2 className="font-bold text-7xl">Registrasi</h2>
 				<RegisterForm />
-				<span className="font-light text-xs">
+				<span className="font-light text-xs text-center">
 					Sudah punya akun?{" "}
 					<Link href="/login" className="hover:underline font-bold text-xs">
 						Login Sekarang!
