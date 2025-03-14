b3f4e8e6cc9dcd6959352b9949cf4d9dd8ed4c01
ahargunyllib <billy.bpm03@gmail.com>
2025-03-14 23:19:03 +0700

    feat: add mobile menu toggle functionality to Header component

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import {
-	LineiconsSearch2,
 	LucideLogOut,
+	LucideX,
 	MaterialSymbolsPerson2,
 	MdiHome,
 	MingcuteBookLine,
 	RiCustomerService2Fill,
 } from "@/shared/components/icons";
+import { LucideMenu } from "@/shared/components/icons/lucide-menu";
+import Button from "@/shared/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
 	const router = useRouter();
@@ -35,6 +39,8 @@ export default function Header() {
 		// 	icon: <LineiconsSearch2 fontSize={24} />,
 		// },
 	];
+
+	const [isOpen, setOpen] = useState(false);
 
 	return (
 		<div className="py-6 px-4 sm:px-20 w-full bg-primary sticky top-0 z-[999] flex flex-row justify-between items-center">
@@ -78,6 +84,38 @@ export default function Header() {
 						</div>
 					);
 				})}
+			</div>
+			<div className="flex w-12 shrink lg:hidden items-end justify-end">
+				<Button
+					variant="ghost"
+					onClick={() => setOpen(!isOpen)}
+					className="p-0 m-0"
+				>
+					{isOpen ? (
+						<LucideX fontSize={24} className="text-white" />
+					) : (
+						<LucideMenu fontSize={24} className="text-white" />
+					)}
+				</Button>
+				{isOpen && (
+					<div className="absolute top-28 flex flex-col w-full right-0 bg-primary shadow-lg py-4 gap-8">
+						{routes.map((route) => (
+							<div
+								key={route.name}
+								className="flex flex-col gap-2 px-4 sm:px-20 hover:tracking-wider transition-all duration-300 ease-in-out"
+							>
+								<Link
+									href={route.href}
+									className="flex justify-between items-center"
+								>
+									<span className="text-lg font-bold text-white">
+										{route.name}
+									</span>
+								</Link>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 		</div>
 	);
