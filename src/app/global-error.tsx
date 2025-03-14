b1826c269a2d87f9ea8a81a91e7cf31c78b5a209
ahargunyllib <billy.bpm03@gmail.com>
2025-03-15 04:44:06 +0700

    feat: enhance error and not found pages with user-friendly messages and layout

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,5 +3,19 @@
 export default function GlobalError({
 	error,
 }: { error: Error & { digest?: string } }) {
-	return <div>{error.message}</div>;
+	return (
+		<section className="flex items-center justify-center h-screen px-16">
+			<div className="px-12 py-12 flex flex-col items-center justify-center gap-y-12 rounded-3xl border max-w-[32rem]">
+				<div className="space-y-6 text-center">
+					<h2 className="text-mobile-title font-extrabold">
+						Oops, Terjadi Kesalahan!
+					</h2>
+					<p className="text-muted-foreground">
+						Sepertinya terjadi kesalahan pada server. Mohon coba beberapa saat
+						lagi.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,24 @@
+import Button from "@/shared/components/ui/button";
+import Link from "next/link";
+
 export default function Page() {
-	return <div>Not Found</div>;
+	return (
+		<section className="flex items-center justify-center h-screen px-16">
+			<div className="px-12 py-12 flex flex-col items-center justify-center gap-y-12 rounded-3xl border max-w-[32rem]">
+				<div className="space-y-6 text-center">
+					<h2 className="text-mobile-title font-extrabold">
+						Oops, Halaman tidak ditemukan!
+					</h2>
+					<p className="text-muted-foreground">
+						Sepertinya halaman yang anda sedang cari tidak ada. Jangan khawatir,
+						mari kembali ke laman sebelumnya.
+					</p>
+				</div>
+
+				<Link href="/">
+					<Button className="w-fit">Kembali</Button>
+				</Link>
+			</div>
+		</section>
+	);
 }
