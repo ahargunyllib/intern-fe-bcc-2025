08e21c1646588f64edc57653322ae2e9f66c1d91
ahargunyllib <billy.bpm03@gmail.com>
2025-03-14 23:28:27 +0700

    feat: enhance TrendingPostPanel with user profile display and image integration

--- src/features/profile/components/trending-post-panel.tsx
+++ src/features/profile/components/trending-post-panel.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function TrendingPostPanel() {
 	return (
 		<div className="p-4 space-y-4 bg-white shadow-md rounded-md flex flex-col">
@@ -6,8 +8,21 @@ export default function TrendingPostPanel() {
 				<div
 					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 					key={index}
-					className="bg-gray-400 hover:bg-gray-400/80 transition-colors duration-300 ease-in-out cursor-pointer h-40"
-				/>
+					className="flex flex-col gap-2 hover:cursor-pointer"
+				>
+					<div className="flex flex-row items-center gap-2">
+						<div className="relative size-8">
+							<Image
+								src={`https://picsum.photos/seed/${Math.random() * 100}/64/64`}
+								alt="Profile Picture"
+								fill
+								className="rounded-full"
+							/>
+						</div>
+						<span className="font-semibold">displayName</span>
+					</div>
+					<div className="bg-gray-400 hover:bg-gray-400/80 transition-colors duration-300 ease-in-out h-40" />
+				</div>
 			))}
 		</div>
 	);
