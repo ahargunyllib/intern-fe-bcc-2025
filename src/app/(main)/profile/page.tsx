f9a67725b1c7c220a1967bed960fcf6abe6ae88f
ahargunyllib <billy.bpm03@gmail.com>
2025-03-14 19:32:53 +0700

    feat: add CreatePostForm component for post creation and integrate it into profile page

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,5 +1,13 @@
+import CreatePostForm from "@/features/profile/components/create-post-form";
 import ProfileContainer from "@/features/profile/containers/profile-container";
 
 export default function Page() {
-	return <ProfileContainer />;
+	return (
+		<div className="relative h-screen">
+			<ProfileContainer />
+			<div className="w-full h-full fixed inset-0">
+				<CreatePostForm />
+			</div>
+		</div>
+	);
 }
---
+++ src/features/profile/components/create-post-form.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { MaterialSymbolsPostAdd } from "@/shared/components/icons";
+import Button from "@/shared/components/ui/button";
+import Textarea from "@/shared/components/ui/textarea";
+import { useModalStore } from "@/shared/stores/use-modal-store";
+
+export default function CreatePostForm() {
+	const { openModal } = useModalStore();
+
+	return (
+		<Button
+			className="rounded-full size-20 absolute right-4 bottom-4"
+			onClick={() =>
+				openModal({
+					children: (
+						<div className="space-y-2 z-50">
+							<Textarea className="w-full" placeholder="Ketik disini" />
+							<div className="flex justify-end gap-2">
+								<Button>Unggah</Button>
+							</div>
+						</div>
+					),
+					title: "Buat Postingan",
+				})
+			}
+			onKeyDown={() => {}}
+		>
+			<MaterialSymbolsPostAdd fontSize={40} />
+		</Button>
+	);
+}
